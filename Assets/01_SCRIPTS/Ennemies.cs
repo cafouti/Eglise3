da26df0a92eda5969e283520c28b2bf7aff039b2
LilianG28 <gurruchagalilian@gmail.com>
2021-03-04 15:52:55 +0100

    Ajout zigouilleur

--- Assets/01_SCRIPTS/Ennemies.cs
+++ Assets/01_SCRIPTS/Ennemies.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Ennemies : MonoBehaviour
 {
     public float speed;
-    private float masse;
+    public float masse;
     private float gravity;
+    private bool poursuite = false;
 
     private GameObject character;
     private CharacterController controller;
     private Vector3 mouvement;
     private BoxCollider detectionZone;
+    private Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +26,67 @@ public class Ennemies : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouvement.x = speed;
+
+        if (controller.isGrounded && mouvement.y < 0)
+        {
+            mouvement.y = masse;
+        }
+
+        if (poursuite)
+        {
+            if (character.transform.position.x > player.transform.position.x)
+            {
+                mouvement.x = -speed;
+            }
+            else if (character.transform.position.x < player.transform.position.x)
+            {
+                mouvement.x = speed;
+            }
+        }
+        else
+        {
+            mouvement.x = 0;
+        }
+        
+        Debug.Log("Mouvement = " + mouvement);
+
         controller.Move(mouvement * Time.deltaTime);
     }
     
     void Rotate()
     {
         character.transform.Rotate(0,180,0);
+    }
+
+    void Poursuite(float x)
+    {        
+            if(character.transform.position.x > transform.position.x)
+            {
+                x = speed;
+            }
+            else if(character.transform.position.x > transform.position.x)
+            {
+                x = -speed;
+            }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.name == "Fille(Clone)" || other.gameObject.name == "Monstre(Clone)")
+        {
+            Debug.Log(other.gameObject.name);
+            player = other.gameObject.transform;
+            poursuite = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Fille(Clone)" || other.gameObject.name == "Monstre(Clone)")
+        {
+            player = null;
+            //poursuite = false;
+        }
     }
 }
